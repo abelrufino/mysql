8d55d9809f695a7738471138312019f88f144355
Abel Neto <anetosib@gmail.com>
2021-09-28 22:49:13 -0300

    Pratica02 Mysql

---
+++ pratica2.sql
@@ -0,0 +1,39 @@
+USE pratica01;
+
+# DML
+
+-- Insert 
+INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES ('Notebook', '1200', 1);
+INSERT INTO produtos VALUES (NULL, 'Macbook Pro', '7200', 1);
+
+INSERT INTO tipos_produto (descricao) VALUES ('Apple');
+INSERT INTO produtos VALUES (NULL, 'Macbook Air', '5200', 3);
+
+SELECT * FROM produtos;
+SELECT * FROM tipos_produto;
+
+-- Update
+-- Cuidado Update (sem o WHERE)
+UPDATE produtos SET codigo_tipo = 3 WHERE codigo = 6;
+UPDATE produtos SET descricao = 'Impressora Laser Epson', preco = '700' WHERE codigo = 4;
+
+
+UPDATE produtos SET preco = '250'; 
+UPDATE produtos SET preco = '250' WHERE codigo < 4;
+
+UPDATE produtos SET preco = '2800' WHERE codigo = 5;
+	
+-- Delete 
+-- Cuidado Delete (sem o WHERE)
+DELETE FROM produtos WHERE codigo = 3; 
+
+
+
+
+
+
+
+
+
+
+
