0fd68cb136d008a7be95cbd7d6c1e39273070957
Abel Neto <anetosib@gmail.com>
2021-10-03 22:48:45 -0300

    Praticas05_2_2Mysql

--- pratica05_2.sql
+++ pratica05_2.sql
@@ -5,3 +5,13 @@ SELECT * FROM produtos;
 
 SELECT p.codigo, p.descricao, p.preco, tp.descricao FROM produtos p, tipos_produto tp 
 WHERE p.codigo_tipo = tp.codigo;
+
+
+
+# Consultas em multiplas tabelas
+
+SELECT * FROM tipos_produto; 
+SELECT * FROM produtos;
+
+SELECT p.codigo AS 'Código', p.descricao AS 'Descrição', p.preco AS 'Preço', tp.descricao AS 'Tipo Produto'FROM produtos p, tipos_produto tp 
+WHERE p.codigo_tipo = tp.codigo;
